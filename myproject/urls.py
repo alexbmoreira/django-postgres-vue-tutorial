790741aee364f4c3d93059aab255f3ae1ed7c033
alexbmoreira <alexbmoreira@gmail.com>
2021-01-01 15:56:39 -0500

    Added templates directory and appropriate URLs

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('films.urls'))
+    path('api/', include('films.urls')),
+    re_path(r'^(?!api(/)?|admin(/)?).*$', TemplateView.as_view(template_name="index.html"), name="entry-point")
 ]
