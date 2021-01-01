fd178c3be003c587572eee1a5b639ce925556d98
alexbmoreira <alexbmoreira@gmail.com>
2021-01-01 15:44:56 -0500

    Added models

--- films/models.py
+++ films/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Film(models.Model):
+    title = models.CharField(max_length=120)
+    release = models.DateField()
+    runtime = models.IntegerField()
+    director = models.ForeignKey("Director", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.release})"
