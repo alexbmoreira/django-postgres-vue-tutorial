4ec5fe3e9287c20ba2adf1b0cfaa8250d7864daf
alexbmoreira <alexbmoreira@gmail.com>
2021-01-01 16:27:13 -0500

    Added custom font

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/css/tailwind.css";
+import "./assets/css/fonts.css";
 
 Vue.config.productionTip = false;
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'quicksand': ['Quicksand', 'sans-serif']
+    },
     screens: {
       '2xl': {'max': '1535px'},
       // => @media (max-width: 1535px) { ... }
