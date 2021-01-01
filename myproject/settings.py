51169d6da729d9f2319f211ecbb1b1786f743ef7
alexbmoreira <alexbmoreira@gmail.com>
2021-01-01 15:34:19 -0500

    Configured DJango to use postgreSQL

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for myproject project.
 
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': '5432',
     }
 }
 
